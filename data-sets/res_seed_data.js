21b0090769e95a4a4ef6ec5693eab2b2688576ac
jciaramella <james_ciaramella@hotmail.co.uk>
2018-05-01 10:46:08 +0100

    created sql-data-restaurants.txt file containing all of the restaurant data

---
+++ data-sets/res_seed_data.js
@@ -0,0 +1,20 @@
+const fs = require('fs-extra')
+
+const restaurantsArray = []
+fs.readJson(`./restaurants.json`)
+    .then(data => {
+        return data
+    })
+    .then(resData => {
+        const sqlInsert = "INSERT INTO restaurants (restaurant_name, restaurant_address, restaurant_phone, restaurant_URL, restaurant_longitude, restaurant_latitude, restaurant_price)"
+        let sqlValues = ""
+
+        for(let i = 0; i < resData.businesses.length; i++){
+            const {name, phone, url, coordinates, price, location} = resData.businesses[i]
+            sqlValues += `VALUES ('${name}','${location.display_address}','${phone}','${url}','${coordinates.longitude}','${coordinates.latitude}', '${price}'),`
+        }
+        return sqlInsert + sqlValues
+    })
+    .then(sqlData => {
+        fs.writeFile('./sql-data-restaurants.txt', sqlData)
+    })
